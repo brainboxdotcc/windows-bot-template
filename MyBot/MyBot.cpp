6e0ef16a178a4ad79c5d0cbc847fe699d4dfbc4f
Craig Edwards <craigedwards@brainbox.cc>
2021-09-24 13:50:23 +0100

    add logger

--- MyBot/MyBot.cpp
+++ MyBot/MyBot.cpp
@@ -3,16 +3,20 @@
 
 int main()
 {
-    dpp::cluster bot("token");
+    dpp::cluster bot("put your bot token here");
 
     bot.on_ready([&bot](const dpp::ready_t& event) {
-        std::cout << "Logged in as " << bot.me.username << "!\n";
+        bot.log(dpp::ll_info, "Logged in as " + bot.me.username);
     });
 
     bot.on_message_create([&bot](const dpp::message_create_t& event) {
         if (event.msg->content == "!ping") {
             bot.message_create(dpp::message(event.msg->channel_id, "Pong!"));
         }
+    });
+
+    bot.on_log([](const dpp::log_t& event) {
+        std::cout << event.message << "\n";
     });
 
     bot.start(false); 
