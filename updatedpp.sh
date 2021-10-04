6852d42e0a0cf8f79a0b53ae4b74a98a5cd6b8aa
brain <craigedwards@brainbox.cc>
2021-10-04 15:53:36 +0100

    fix: codacy adustments

--- updatedpp.sh
+++ updatedpp.sh
@@ -17,13 +17,13 @@
 #
 echo "Fetching latest DPP"
 mkdir temp
-cd temp
+cd temp || exit
 echo "Download assets from CI..."
 gh run list -w "D++ CI" -R "brainboxdotcc/DPP" | grep $'\t'master$'\t' | grep ^completed | head -n1
 gh run download -R "brainboxdotcc/DPP" $(gh run list -w "D++ CI" -R "brainboxdotcc/DPP" | grep $'\t'master$'\t' | grep ^completed | head -n1 | awk '{ printf $(NF-2) }')
 
 echo "Process windows x64 release"
-cd "./libdpp - Windows x64-Release" && unzip -qq *.zip
+cd "./libdpp - Windows x64-Release" && unzip -qq ./*.zip
 # header files from first zip
 cp -rv ./*/include ../../MyBot/dependencies/
 
@@ -34,7 +34,7 @@ cp -rv ./*/lib ../../MyBot/dependencies/64/release/
 cd .. || exit
 
 echo "Process windows x64 debug"
-cd "./libdpp - Windows x64-Debug" && unzip -qq *.zip
+cd "./libdpp - Windows x64-Debug" && unzip -qq ./*.zip
 # dll files
 cp -rv ./*/bin/*.dll ../../MyBot/dependencies/64/debug/bin/
 # lib files
@@ -42,7 +42,7 @@ cp -rv ./*/lib ../../MyBot/dependencies/64/debug/
 cd .. || exit
 
 echo "Process windows x86 release"
-cd "./libdpp - Windows x86-Release" && unzip -qq *.zip
+cd "./libdpp - Windows x86-Release" && unzip -qq ./*.zip
 # dll files
 cp -rv ./*/bin/*.dll ../../MyBot/dependencies/32/release/bin/
 # lib files
@@ -50,7 +50,7 @@ cp -rv ./*/lib ../../MyBot/dependencies/32/release/
 cd .. || exit
 
 echo "Process windows x86 debug"
-cd "./libdpp - Windows x86-Debug" && unzip -qq *.zip
+cd "./libdpp - Windows x86-Debug" && unzip -qq ./*.zip
 # dll files
 cp -rv ./*/bin/*.dll ../../MyBot/dependencies/32/debug/bin/
 # lib files
@@ -60,7 +60,7 @@ cd .. || exit
 echo "Converting linefeeds from dos to unix"
 # dos2unix
 cd .. || exit
-cd MyBot/dependencies/include/dpp-9.0/dpp
+cd MyBot/dependencies/include/dpp-9.0/dpp || exit
 find . -exec dos2unix {} \;
 cd ../../../../.. || exit
 find . -name '*.cmake' -exec dos2unix -q {} \;
