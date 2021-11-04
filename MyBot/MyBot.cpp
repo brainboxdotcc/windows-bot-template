8f52d51351f2d35ce60359436c70e15afe52bc37
Craig Edwards <craigedwards@brainbox.cc>
2021-11-04 10:41:06 +0000

    feat: add exception handling to make this more user friendly feat: make it so that you have to read a comment and uncomment a line to make the source build

--- MyBot/MyBot.cpp
+++ MyBot/MyBot.cpp
@@ -3,25 +3,32 @@
 
 int main()
 {
-    dpp::cluster bot("put your bot token here");
+    try {
+        // Be sure to place your token in the line below, and uncomment the line
+        // Follow steps here to get a token: https://dpp.dev/creating-a-bot-application.html
+        //dpp::cluster bot("place token here");
 
-    bot.on_ready([&bot](const dpp::ready_t& event) {
-        bot.log(dpp::ll_info, "Logged in as " + bot.me.username);
-    });
+        bot.on_ready([&bot](const dpp::ready_t& event) {
+            bot.log(dpp::ll_info, "Logged in as " + bot.me.username);
+        });
 
-    bot.on_message_create([&bot](const dpp::message_create_t& event) {
-        if (event.msg->content == "!ping") {
-            bot.message_create(dpp::message(event.msg->channel_id, "Pong!"));
-        }
-    });
+        bot.on_message_create([&bot](const dpp::message_create_t& event) {
+            if (event.msg->content == "!ping") {
+                bot.message_create(dpp::message(event.msg->channel_id, "Pong!"));
+            }
+        });
 
-    bot.on_log([](const dpp::log_t& event) {
-        if (event.severity > dpp::ll_trace) {
-            std::cout << dpp::utility::loglevel(event.severity) << ": " << event.message << "\n";
-        }
-    });
+        bot.on_log([](const dpp::log_t& event) {
+            if (event.severity > dpp::ll_trace) {
+                std::cout << dpp::utility::loglevel(event.severity) << ": " << event.message << "\n";
+            }
+        });
 
-    bot.start(false); 
+        bot.start(false);
+    }
+    catch (const std::exception& e) {
+        std::cout << "Error: " << e.what() << "\n";
+    }
 
     return 0;
 }
