c88dbd87053030e97e871cec385aa5e0177c4f99
Craig Edwards <braindigitalis@users.noreply.github.com>
2022-05-20 17:20:38 +0100

    Update MyBot.cpp

--- MyBot/MyBot.cpp
+++ MyBot/MyBot.cpp
@@ -1,13 +1,13 @@
 #include <dpp/dpp.h>
 
 /* Be sure to place your token in the line below.
- * Follow steps here to get a token: https://dpp.dev/creating-a-bot-application.html
- * You will also need to fill in your guild id. When you invite the bot, be sure to
- * invite it with the scopes 'bot' and 'applications.commands', e.g.
+ * Follow steps here to get a token:
+ * https://dpp.dev/creating-a-bot-application.html
+ * When you invite the bot, be sure to invite it with the 
+ * scopes 'bot' and 'applications.commands', e.g.
  * https://discord.com/oauth2/authorize?client_id=940762342495518720&scope=bot+applications.commands&permissions=139586816064
  */
 const std::string    BOT_TOKEN    = "add your token here";
-const dpp::snowflake MY_GUILD_ID  =  825407338755653642;
 
 int main()
 {
@@ -18,7 +18,7 @@ int main()
     bot.on_log(dpp::utility::cout_logger());
 
     /* Handle slash command */
-    bot.on_interaction_create([](const dpp::interaction_create_t& event) {
+    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
          if (event.command.get_command_name() == "ping") {
             event.reply("Pong!");
         }
@@ -28,7 +28,7 @@ int main()
     bot.on_ready([&bot](const dpp::ready_t& event) {
         /* Wrap command registration in run_once to make sure it doesnt run on every full reconnection */
         if (dpp::run_once<struct register_bot_commands>()) {
-            bot.guild_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id), MY_GUILD_ID);
+            bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
         }
     });
 
