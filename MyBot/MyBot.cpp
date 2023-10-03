87f35f4261828ced0a6d53b8cfa382cdd161a2e2
Archie Jaskowicz <archiejaskowicz@gmail.com>
2023-10-03 13:28:24 +0100

    fix: corrected indentation in MyBot.cpp (#5)

--- MyBot/MyBot.cpp
+++ MyBot/MyBot.cpp
@@ -12,29 +12,29 @@ const std::string    BOT_TOKEN    = "add your token here";
 
 int main()
 {
-    /* Create bot cluster */
-    dpp::cluster bot(BOT_TOKEN);
+	/* Create bot cluster */
+	dpp::cluster bot(BOT_TOKEN);
 
-    /* Output simple log messages to stdout */
-    bot.on_log(dpp::utility::cout_logger());
+	/* Output simple log messages to stdout */
+	bot.on_log(dpp::utility::cout_logger());
 
-    /* Handle slash command */
-    bot.on_slashcommand([](const dpp::slashcommand_t& event) {
-         if (event.command.get_command_name() == "ping") {
-            event.reply("Pong!");
-        }
-    });
+	/* Handle slash command */
+	bot.on_slashcommand([](const dpp::slashcommand_t& event) {
+		if (event.command.get_command_name() == "ping") {
+			event.reply("Pong!");
+		}
+	});
 
-    /* Register slash command here in on_ready */
-    bot.on_ready([&bot](const dpp::ready_t& event) {
-        /* Wrap command registration in run_once to make sure it doesnt run on every full reconnection */
-        if (dpp::run_once<struct register_bot_commands>()) {
-            bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
-        }
-    });
+	/* Register slash command here in on_ready */
+	bot.on_ready([&bot](const dpp::ready_t& event) {
+		/* Wrap command registration in run_once to make sure it doesnt run on every full reconnection */
+		if (dpp::run_once<struct register_bot_commands>()) {
+			bot.global_command_create(dpp::slashcommand("ping", "Ping pong!", bot.me.id));
+		}
+	});
 
-    /* Start the bot */
-    bot.start(dpp::st_wait);
+	/* Start the bot */
+	bot.start(dpp::st_wait);
 
-    return 0;
+	return 0;
 }
